852124ac154205602f1586dbd7de9cea2f88273b
fernando850 <f.a.martinsfernando@gmail.com>
2025-06-06 15:44:47 -0700

    Fix invalid hrefs in Login.js to pass ESLint and Netlify build

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,19 +5,16 @@ import '../styles/Login.css';
 
 const Login = () => {
   return (
-
     <>
-        <Navbar />
-        
-        <div className="login-page">
-        
+      <Navbar />
 
+      <div className="login-page">
         <div className="login-container">
-            <h2 className="login-header">Login</h2>
-            <img src="/Images/logoRelaxio.png" alt="Relaxio Logo" className="login-logo" />
-            <h3 className="signin-title">Sign in</h3>
+          <h2 className="login-header">Login</h2>
+          <img src="/Images/logoRelaxio.png" alt="Relaxio Logo" className="login-logo" />
+          <h3 className="signin-title">Sign in</h3>
 
-            <form className="login-form">
+          <form className="login-form">
             <label htmlFor="email">Email address:</label>
             <input type="email" id="email" name="email" />
 
@@ -25,47 +22,35 @@ const Login = () => {
             <input type="password" id="password" name="password" />
 
             <div className="login-options">
-            <label>
+              <label>
                 <input type="checkbox" />
                 <span className="checkmark"></span>
                 Remember me
-            </label>
-                <a href="#">Forgot your password?</a>
+              </label>
+              <a href="/support">Forgot your password?</a>
             </div>
 
             <div className="center-button">
-                <button type="submit" className="login-button">Sign In</button>
-                <p className="signup-link">New to relaxio? <a href="#">Create an account</a>
-            </p>
+              <button type="submit" className="login-button">Sign In</button>
+              <p className="signup-link">
+                New to Relaxio? <a href="/signin">Create an account</a>
+              </p>
             </div>
-           
-            </form>
+          </form>
 
-           
-
-            <p className="login-terms">
-            This site is protected by reCAPTCHA and the Google <a href="#">Privacy Policy</a> and <a href="#">Terms of Service</a> apply.
-            </p>
+          <p className="login-terms">
+            This site is protected by reCAPTCHA and the Google <a href="/support">Privacy Policy</a> and <a href="/support">Terms of Service</a> apply.
+          </p>
         </div>
-
 
         <div className="footer-buttons">
-            <FooterButtons />
-        </div>
-        
-        <div className='logo-bottom'>
-            <LogoBottom />
+          <FooterButtons />
         </div>
 
+        <div className="logo-bottom">
+          <LogoBottom />
         </div>
-
-        
-        
-
-        
-
-        
-        
+      </div>
     </>
   );
 };
